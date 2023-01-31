0de3738b22baafaf7b3aec1862b12931657a07cb
robertvlad <vladrob96@gmail.com>
2023-01-31 16:16:12 +0100

    Milestone 1: Struttura del tool base

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+
+    $lunghezza_password = $_POST['password'] ?? '';
+
+    // FUNZIONE CONTENETE ARRAY CON TUTTI I POSSIBILI SIMBOLI CHE COMPORRANNO LA PASSWORD FINALE A SECONDA DELLA LUNGHEZZA
+    function passwordRandom($lunghezza_password) {
+
+        $array_lettere = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        $array_numeri = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $array_simboli = ['!', '"', '£', '$', '%', '&', '/', '(', ')', '=', '?', '^', '*', '[', ']'];
+
+        $array_totale = array_merge($array_lettere, $array_numeri, $array_simboli);
+
+        $lunghezza_tot = count($array_totale) - 1;
+
+        for($i= 0; $i < $lunghezza_password; $i++) {
+            $n = rand(0, $lunghezza_tot);
+            $pass[] = $array_totale[$n]; 
+        }
+        return implode($pass);
+    }
+
+    // AVVISO NEL CASO L'UTENTE NON ABBIA SELEZIONATO LA LUNGHEZZA DELLA PASSWORD
+    if($lunghezza_password === '') {
+        $pass_generata = 'Devi inserire una lunghezza per la password!';
+    } else {
+        $pass_generata = passwordRandom($lunghezza_password);
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+        <!-- TITLE -->
+        <title>Password Generator</title>
+    </head>
+
+    <body>
+        <div class="container">
+
+            <!-- FORM CON LE DIVERSE OPZIONI -->
+            <form action="index.php" method="POST">
+                <div class="mb-3 d-flex">
+                    <div>
+                        <p>Lunghezza Password:</p>
+                    </div>
+                    <div>
+                        <input type="number" class="form-control" name="password">
+                    </div>
+                </div>
+                <div class="mb-3 d-flex">
+                    <div>
+                        <p>Consenti ripetizioni di uno o più caratteri:</p>
+                    </div>
+                <div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" checked>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Si
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault">
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        No
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Lettere
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="">
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Numeri
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="">
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Simboli
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <!-- TASTI START/STOP -->
+        <button type="submit" class="btn btn-primary">Invia</button>
+        <button type="reset" class="btn btn-secondary">Annulla</button>
+
+        <!-- PASSWORD GENERATA -->
+        </form>
+            <div>
+                <h3>La tua password è: <?php echo $pass_generata; ?></h3>
+            </div>
+        </div>
+    </body>
+</html>
