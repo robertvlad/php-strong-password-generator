97c5ecefdd08f2c0e8444ee4383784dc8ee6d259
robertvlad <vladrob96@gmail.com>
2023-01-31 16:21:41 +0100

    Milestone 2: logica spostata nel file functions.php

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
 <?php
 
+    // LINK ALLE FUNCTIONS
+    include __DIR__ .'./functions/functions.php';
+
     $lunghezza_password = $_POST['password'] ?? '';
-
-    // FUNZIONE CONTENETE ARRAY CON TUTTI I POSSIBILI SIMBOLI CHE COMPORRANNO LA PASSWORD FINALE A SECONDA DELLA LUNGHEZZA
-    function passwordRandom($lunghezza_password) {
-
-        $array_lettere = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        $array_numeri = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        $array_simboli = ['!', '"', '£', '$', '%', '&', '/', '(', ')', '=', '?', '^', '*', '[', ']'];
-
-        $array_totale = array_merge($array_lettere, $array_numeri, $array_simboli);
-
-        $lunghezza_tot = count($array_totale) - 1;
-
-        for($i= 0; $i < $lunghezza_password; $i++) {
-            $n = rand(0, $lunghezza_tot);
-            $pass[] = $array_totale[$n]; 
-        }
-        return implode($pass);
-    }
 
     // AVVISO NEL CASO L'UTENTE NON ABBIA SELEZIONATO LA LUNGHEZZA DELLA PASSWORD
     if($lunghezza_password === '') {
@@ -26,6 +11,9 @@
     } else {
         $pass_generata = passwordRandom($lunghezza_password);
     }
+
+    // CHIAMATA FUNZIONE
+    passwordRandom($lunghezza_password);
 
 ?>
 
